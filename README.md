b627ee7ca12f34d182b651166b0893790516cabe
Kirill Bobykin <qelphybox@gmail.com>
2021-05-01 20:50:56 +0300

    add config

---
+++ README.md
@@ -0,0 +1,10 @@
+# Youtube stas bot
+
+Telegram bot manages youtube playlist from chat
+
+## Run
+
+```bash
+cat "TELEGRAM_BOT_TOKEN=yourtoken" > .env
+npm start
+```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
+bot.onText(/\/start/, (msg) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = "Hello, im Stas 👋, use /follow to rock'n'roll 🤘"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
