3da346b68a053278374a2445cd8c47563834252e
Vasiliy Bolgar <reanimatorzon@users.noreply.github.com>
2021-05-10 17:44:48 +0300

    use self-signed ssl for heroku postgres

--- src/db.js
+++ src/db.js
@@ -6,6 +6,7 @@ class DB {
   constructor(uri) {
     this.client = new Client({
       connectionString: uri,
+      ssl: process.env.DATABASE_SSL === 'false' ? false : { rejectUnauthorized: false }
     })
   }
 
